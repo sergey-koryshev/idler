35030b0dd416c42df4deb8040cfbf7239e782253
Ace Lightning <ace-lightning@outlook.com>
2020-06-02 12:10:29 +0300

    feat (VMMVHelper): implemented helper class VMMVHelper

---
+++ Idler/VMMVHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idler
+{
+    public abstract class VMMVHelper : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
