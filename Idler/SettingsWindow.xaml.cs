bba20d2c00eeb5827229d38336643d1f08db047b
Ace Lightning <ace-lightning@outlook.com>
2020-06-26 12:39:56 +0300

    feat (SettingsWindow): added window Settings

---
+++ Idler/SettingsWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Idler.Properties;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Idler
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window, INotifyPropertyChanged
+    {
+        private NoteCategories noteCategories;
+        public NoteCategories NoteCategories
+        {
+            get => noteCategories;
+            set
+            {
+                noteCategories = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.NoteCategories)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+        public SettingsWindow(NoteCategories noteCategories) : this()
+        {
+            this.NoteCategories = noteCategories;
+        }
+
+        private async void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.Reload();
+            await this.NoteCategories.RefreshAsync();
+        }
+
+        private async void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.Save();
+            await this.NoteCategories.UpdateAsync();
+        }
+
+        private void btnDataSourceOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == true)
+            {
+                this.txtDataSource.Text = openFile.FileName;
+            }
+        }
+
+        private void btnDefault_Click(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.Reset();
+        }
+    }
+}
