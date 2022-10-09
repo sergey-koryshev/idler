efea8f83e31fd93c3b4b23819101a073a9eaa4da
Sergey Koryshev <sergey.koryshev@akvelon.com>
2022-10-09 15:16:47 +0200

    implemented class BaseViewModel

---
+++ src/Idler/ViewModels/BaseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Idler.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Occurs when any property has been changed except of property "Changed"
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Notifies subscribers that specified property has been changed
+        /// </summary>
+        /// <param name="propertyName">(Optional) Name of property</param>
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
