f0b0beb2cbc56d67c834451ff964e72eda701e5c
Sergey Koryshev <sergey.koryshev@akvelon.com>
2022-10-24 00:59:37 +0200

    Bumped version to 1.2.3.15

--- src/Idler/Properties/AssemblyInfo.cs
+++ src/Idler/Properties/AssemblyInfo.cs
@@ -41,5 +41,5 @@ using System.Windows;
 )]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.2.2.15")]
-[assembly: AssemblyInformationalVersion("1.2.2.15")]
+[assembly: AssemblyFileVersion("1.2.3.15")]
+[assembly: AssemblyInformationalVersion("1.2.3.15")]
