613296eacad432397c1909118c022dea37a0cfa4
sergey-koryshev <sergey-koryshev@users.noreply.github.com>
2022-10-23 23:17:47 +0000

    [automated] Bumped version to 1.2.3.17 [skip ci]

--- src/Idler/Properties/AssemblyInfo.cs
+++ src/Idler/Properties/AssemblyInfo.cs
@@ -41,5 +41,5 @@ using System.Windows;
 )]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.2.3.16")]
-[assembly: AssemblyInformationalVersion("1.2.3.16")]
+[assembly: AssemblyFileVersion("1.2.3.17")]
+[assembly: AssemblyInformationalVersion("1.2.3.17")]
