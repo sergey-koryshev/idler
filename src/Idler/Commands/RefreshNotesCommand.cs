0c815ad3d96db071a1427f01cc2e68492126189e
Sergey Koryshev <sergey.koryshev@outlook.com>
2023-08-08 08:40:21 +0200

    added command RefreshNotesCommand

---
+++ src/Idler/Commands/RefreshNotesCommand.cs
@@ -0,0 +1,36 @@
+﻿using Idler.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Idler.Commands
+{
+    public class RefreshNotesCommand : CommandBase
+    {
+        private Shift shift;
+
+        public RefreshNotesCommand(Shift shift) {
+            this.shift = shift;
+        }
+        public override void Execute(object parameter)
+        {
+            bool canRefresh = true;
+
+            if (this.shift.Changed)
+            {
+                canRefresh = MessageBox.Show(
+                    "There are unsaved changes, are you sure you want to refresh without saving?",
+                    "Warning",
+                    MessageBoxButton.OKCancel) == MessageBoxResult.OK;
+            }
+
+            if (canRefresh)
+            {
+                this.shift.RefreshAsync().SafeFireAndForget();
+            }
+        }
+    }
+}
