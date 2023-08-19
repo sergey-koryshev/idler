993158be6a4547e5746fc03462bfeb66ff7896ee
Sergey Koryshev <sergey.koryshev@outlook.com>
2023-08-19 19:21:48 +0200

    added converter height to corner radius

---
+++ src/Idler/Converters/HeightToCornerRadius.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace Idler.Converters
+{
+    public class HeightToCornerRadius : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double height = (double)value;
+            return height / 2;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return new NotImplementedException();
+        }
+    }
+}
