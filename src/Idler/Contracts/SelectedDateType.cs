8721a29b66259bc98dce75a8eef7592ddff981a8
Sergey Koryshev <sergey.koryshev@outlook.com>
2023-08-26 11:23:53 +0200

    added enum SelectedDateType

---
+++ src/Idler/Contracts/SelectedDateType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idler.Contracts
+{
+    public enum SelectedDateType
+    {
+        PreviousDate = -1,
+        NextDate = 1
+    }
+}
