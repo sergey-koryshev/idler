9876ecff2dd8e05c09924eb3f81c07c1ccc76163
Sergey Koryshev <sergey.koryshev@outlook.com>
2023-11-17 07:57:02 +0100

    Added converter which inverts boolean value

---
+++ src/Idler/Converters/BooleanInverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Idler.Converters
+{
+    public class BooleanInverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool booleanValue)
+            {
+                return !booleanValue;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool booleanValue)
+            {
+                return !booleanValue;
+            }
+
+            return null;
+        }
+    }
+}
