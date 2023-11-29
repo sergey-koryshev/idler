034822907b93649dec550ffc321888664187a40b
sergey-koryshev <sergey-koryshev@users.noreply.github.com>
2023-11-29 15:58:08 +0000

    [automated] Bumped version to 1.9.3.62 [skip ci]

--- src/Idler/Properties/AssemblyInfo.cs
+++ src/Idler/Properties/AssemblyInfo.cs
@@ -41,5 +41,5 @@ using System.Windows;
 )]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.9.2.61")]
-[assembly: AssemblyInformationalVersion("1.9.2")]
+[assembly: AssemblyFileVersion("1.9.3.62")]
+[assembly: AssemblyInformationalVersion("1.9.3")]
