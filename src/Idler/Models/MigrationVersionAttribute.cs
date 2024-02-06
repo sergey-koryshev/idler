31a210b5cce33b1b8cebcc503f65ce2761629f41
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-02-06 12:55:39 +0100

    made version in MigrationVersionAttribute be integer

---
+++ src/Idler/Models/MigrationVersionAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idler.Models
+{
+    public class MigrationVersionAttribute : Attribute
+    {
+        public int Version { get; set; }
+
+        public MigrationVersionAttribute(int version)
+        {
+            this.Version = version;
+        }
+    }
+}
