394b7bcf7de347b3dddeb77d0aec3e7d1051b6f3
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-02-11 13:42:46 +0100

    added enum with drag direction

---
+++ src/Idler/Models/DragOverDirection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idler.Models
+{
+    public enum  DragOverDirection
+    {
+        None,
+        Top,
+        Bottom
+    }
+}
