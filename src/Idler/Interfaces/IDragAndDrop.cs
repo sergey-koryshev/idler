9c6b6757dd7f450bd9b82b3ad850af8483fde7c0
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-02-11 13:43:42 +0100

    added direction to IDragAndDrop in method OnElementDropped

--- src/Idler/Interfaces/IDragAndDrop.cs
+++ src/Idler/Interfaces/IDragAndDrop.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Idler.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace Idler.Interfaces
 {
     internal interface IDragAndDrop<T>
     {
-        void OnElementDropped(T droppped, T target);
+        void OnElementDropped(T droppped, T target, DragOverDirection direction);
     }
 }
