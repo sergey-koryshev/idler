1a8aa8b9bc33ed89e696369ea81f5ebfedaa964a
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-02-17 16:43:24 +0100

    added custom TextBox component with overrided methods to handle drag and drop events

---
+++ src/Idler/Components/TextBoxControl/TextBox.cs
@@ -0,0 +1,22 @@
+﻿namespace Idler.Components
+{
+    using System.Windows;
+
+    public class TextBox : System.Windows.Controls.TextBox
+    {
+        protected override void OnDragEnter(DragEventArgs e)
+        {
+            e.Handled = false;
+        }
+
+        protected override void OnDragOver(DragEventArgs e)
+        {
+            e.Handled = false;
+        }
+
+        protected override void OnDragLeave(DragEventArgs e)
+        {
+            e.Handled = false;
+        }
+    }
+}
