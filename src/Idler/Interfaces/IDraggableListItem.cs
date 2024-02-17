ed1ad78513b1b0ee8d4d3c926a0d7c21a39e51db
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-02-17 16:25:26 +0100

    added interface IDraggableListItem

---
+++ src/Idler/Interfaces/IDraggableListItem.cs
@@ -0,0 +1,19 @@
+﻿using Idler.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.HumanInterfaceDevice;
+
+namespace Idler.Interfaces
+{
+    public interface IDraggableListItem
+    {
+        DragOverPlaceholderPosition DragOverPlaceholderPosition { get; set; }
+
+        bool HasDragOver { get; set; }
+
+        int SortOrder { get; set; }
+    }
+}
