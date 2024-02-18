702713d1f3acac1a7acc186d9f0bbe47c4305654
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-02-18 12:19:29 +0100

    refactored custom textbox component

--- src/Idler/Components/TextBoxControl/TextBox.cs
+++ src/Idler/Components/TextBoxControl/TextBox.cs
@@ -2,21 +2,17 @@
 {
     using System.Windows;
 
+    /// <summary>
+    /// Represents custom TextBox with overridden Drag-and-Drop event handlers
+    /// since standard component has built-in logic to handle Drag-and-Drop events and
+    /// it interferes with another drag operations 
+    /// </summary>
     public class TextBox : System.Windows.Controls.TextBox
     {
-        protected override void OnDragEnter(DragEventArgs e)
-        {
-            e.Handled = false;
-        }
+        protected override void OnDragEnter(DragEventArgs e) { }
 
-        protected override void OnDragOver(DragEventArgs e)
-        {
-            e.Handled = false;
-        }
+        protected override void OnDragOver(DragEventArgs e) { }
 
-        protected override void OnDragLeave(DragEventArgs e)
-        {
-            e.Handled = false;
-        }
+        protected override void OnDragLeave(DragEventArgs e) { }
     }
 }
