10626cae6788193b83a58b8c7d75bd8737c61147
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-02-20 08:35:30 +0100

    changed color schema for highlighting

--- src/Idler/Models/TotalEffortType.cs
+++ src/Idler/Models/TotalEffortType.cs
@@ -1,23 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Idler.Models
+﻿namespace Idler.Models
 {
     public enum TotalEffortType
     {
         [DarkerColor(0xaaaaaa)]
         None = 0xffffff,
 
-        [DarkerColor(0xfec500)]
-        Parttime = 0xfff8e0,
+        [DarkerColor(0xf35934)]
+        Parttime = 0xfde4de,
 
         [DarkerColor(0x15d6a0)]
         CompleteShift = 0xc3f9ea,
 
-        [DarkerColor(0xf35934)]
-        Overtime = 0xfde4de
+        [DarkerColor(0xfec500)]
+        Overtime = 0xfff8e0
     }
 }
