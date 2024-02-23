1d73aa90926e315ac4c2f71c9948054042342289
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-02-23 19:28:51 +0100

    extended custom TextBox with new property to set right-sided component inside it

--- src/Idler/Components/TextBoxControl/TextBox.cs
+++ src/Idler/Components/TextBoxControl/TextBox.cs
@@ -2,13 +2,29 @@
 {
     using System.Windows;
 
-    /// <summary>
-    /// Represents custom TextBox with overridden Drag-and-Drop event handlers
-    /// since standard component has built-in logic to handle Drag-and-Drop events and
-    /// it interferes with another drag operations 
-    /// </summary>
     public class TextBox : System.Windows.Controls.TextBox
     {
+        public static readonly DependencyProperty RightControlProperty =
+            DependencyProperty.Register("RightControl", typeof(FrameworkElement), typeof(TextBox), new PropertyMetadata(null));
+
+        public FrameworkElement RightControl
+        {
+            get => (FrameworkElement)GetValue(RightControlProperty);
+            set {
+                SetValue(RightControlProperty, value);
+
+            }
+        }
+
+        static TextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TextBox), new FrameworkPropertyMetadata(typeof(TextBox)));
+        }
+
+        // Drag-and-Drop event handlers are overridden
+        // since standard component has built-in logic
+        // to handle Drag-and-Drop events and
+        // it interferes with another drag operations 
         protected override void OnDragEnter(DragEventArgs e) { }
 
         protected override void OnDragOver(DragEventArgs e) { }
