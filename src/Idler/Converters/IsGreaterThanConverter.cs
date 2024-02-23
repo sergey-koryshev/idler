a8926caf9d03fcb45a9f5aa5b7a9eddfdd393b69
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-02-23 19:29:39 +0100

    added converter to compare if integer is greater than another integer

---
+++ src/Idler/Converters/IsGreaterThanConverter.cs
@@ -0,0 +1,24 @@
+﻿namespace Idler.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    internal class IsGreaterThanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int valueA && parameter is int valueB)
+            {
+                return valueA > valueB;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
