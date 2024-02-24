272f3396ff987dddb78da44951de60be749a1ef8
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-02-24 17:54:19 +0100

    removed ReInstanceCategoryId method from ShiftNote class

--- src/Idler/ShiftNote.cs
+++ src/Idler/ShiftNote.cs
@@ -324,10 +324,5 @@ WHERE {ShiftNote.idFieldName} = ?";
         {
             return $"Shift Note '{this.Description}' ({this.Effort})";
         }
-
-        public void ReInstanceCategoryId()
-        {
-            this.OnPropertyChanged(nameof(this.CategoryId), true);
-        }
     }
 }
