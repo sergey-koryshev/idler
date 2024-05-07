fc63b69047392bdaae2e663c9a56e6c2c0a4ed6a
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-05-07 08:05:09 +0200

    implemented DeleteNotificationCommand

---
+++ src/Idler/Commands/DeleteNotificationCommand.cs
@@ -0,0 +1,25 @@
+﻿using Idler.Helpers.Notifications;
+using Idler.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idler.Commands
+{
+    public class DeleteNotificationCommand : CommandBase
+    {
+        private NotificationViewModel notificationViewModel;
+
+        public DeleteNotificationCommand(NotificationViewModel notificationViewModel)
+        { 
+            this.notificationViewModel = notificationViewModel;
+        }
+
+        public override void Execute(object _)
+        {
+            NotificationsManager.GetInstance().DeleteNotification(notificationViewModel);
+        }
+    }
+}
