977c2d1f0ce1a7d6d25533f512f96e0db631091e
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-05-07 07:49:38 +0200

    added view for notification

---
+++ src/Idler/Views/NotificationView.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Idler.Views
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for NotificationView.xaml
+    /// </summary>
+    public partial class NotificationView : UserControl
+    {
+        public NotificationView()
+        {
+            InitializeComponent();
+        }
+    }
+}
