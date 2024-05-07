27e8082179b59d6b2398689941c70c2430996f44
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-05-07 08:06:54 +0200

    added view model for notification

---
+++ src/Idler/ViewModels/NotificationViewModel.cs
@@ -0,0 +1,53 @@
+﻿namespace Idler.ViewModels
+{
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using Idler.Commands;
+    using Idler.Models;
+
+    public class NotificationViewModel : BaseViewModel
+    {
+        private NotificationType type;
+        private string text;
+        private ICommand deleteNotificationCommand;
+
+        public NotificationType Type
+        { 
+            get => type;
+            set
+            {
+                type = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public string Text
+        { 
+            get => text;
+            set
+            {
+                text = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public ICommand DeleteNotificationCommand
+        {
+            get => deleteNotificationCommand;
+            set
+            {
+                deleteNotificationCommand = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public Visual VisualReference { get; set; }
+
+        public NotificationViewModel(NotificationType type, string text)
+        {
+            this.Type = type;
+            this.Text = text;
+            this.DeleteNotificationCommand = new DeleteNotificationCommand(this);
+        }
+    }
+}
