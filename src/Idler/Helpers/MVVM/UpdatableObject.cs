8293e73251ed176ab100484ede12b71a3587dc75
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-05-10 20:27:57 +0200

    refactored UpdatableObject

--- src/Idler/Helpers/MVVM/UpdatableObject.cs
+++ src/Idler/Helpers/MVVM/UpdatableObject.cs
@@ -1,99 +1,84 @@
-﻿using Idler.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Idler.Helpers.MVVM
+﻿namespace Idler.Helpers.MVVM
 {
-    /// <summary>
-    /// Represents updatable object
-    /// </summary>
+    using System;
+    using System.Threading.Tasks;
+    using Idler.Interfaces;
+
     public abstract class UpdatableObject : ObservableObject, IUpdatable
     {
-        protected bool isRefreashing;
+        protected bool isRefreshing;
 
-        /// <summary>
-        /// Determines if the object is currently refreshing
-        /// </summary>
         public bool IsRefreshing
         {
-            get => isRefreashing;
+            get => isRefreshing;
             set
             {
-                isRefreashing = value;
+                isRefreshing = value;
                 this.OnPropertyChanged();
             }
         }
 
-        /// <summary>
-        /// Occurs when refresh has been completed
-        /// </summary>
         public event EventHandler RefreshCompleted;
 
-        /// <summary>
-        /// Occurs when refresh has been started
-        /// </summary>
         public event EventHandler RefreshStarted;
 
-        /// <summary>
-        /// Occurs when updating has been completed
-        /// </summary>
         public event EventHandler UpdateCompleted;
 
-        /// <summary>
-        /// Occurs when updating has been started
-        /// </summary>
         public event EventHandler UpdateStarted;
 
-        /// <summary>
-        /// Notify that refreshing has been completed
-        /// </summary>
-        protected void OnRefreshCompleted()
+        public async Task RefreshAsync()
+        {
+            try
+            {
+                this.OnRefreshStarted();
+                await this.RefreshInternalAsync();
+                this.Changed = false;
+            }
+            finally
+            {
+                this.OnRefreshCompleted();
+            }
+        }
+
+        public async Task UpdateAsync() {
+            try
+            {
+                this.OnUpdateStarted();
+                await this.UpdateInternalAsync();
+                this.Changed = false;
+            }
+            finally
+            {
+                this.OnUpdateCompleted();
+            }
+        }
+
+        protected abstract Task RefreshInternalAsync();
+
+        protected abstract Task UpdateInternalAsync();
+
+        private void OnRefreshCompleted()
         {
             this.IsRefreshing = false;
             this.RefreshCompleted?.Invoke(this, null);
-            this.Changed = false;
         }
 
-        /// <summary>
-        /// Notify that refreshing has been started
-        /// </summary>
-        protected void OnRefreshStarted()
+        private void OnRefreshStarted()
         {
             this.IsRefreshing = true;
             this.RefreshStarted?.Invoke(this, null);
         }
 
-        /// <summary>
-        /// Notify that updating has been completed
-        /// </summary>
-        protected void OnUpdateCompleted()
+        private void OnUpdateCompleted()
         {
             this.IsRefreshing = false;
             this.UpdateCompleted?.Invoke(this, null);
-            this.Changed = false;
         }
 
-        /// <summary>
-        /// Notify that updating has been started
-        /// </summary>
-        protected void OnUpdateStarted()
+        private void OnUpdateStarted()
         {
             this.IsRefreshing = true;
             this.UpdateStarted?.Invoke(this, null);
         }
-
-        /// <summary>
-        /// Refreshes the object
-        /// </summary>
-        public virtual async Task RefreshAsync() { }
-
-        /// <summary>
-        /// Saves the object
-        /// </summary>
-        public virtual async Task UpdateAsync() { }
     }
 }
