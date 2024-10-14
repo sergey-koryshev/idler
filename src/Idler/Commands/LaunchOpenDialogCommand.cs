2680b2da3dfe99c9cc9c569ffa6c2f7d3aecfb6a
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-10-14 08:55:58 +0200

    added command to launch open dialog

---
+++ src/Idler/Commands/LaunchOpenDialogCommand.cs
@@ -0,0 +1,31 @@
+﻿using Idler.Properties;
+namespace Idler.Commands
+{
+    using System;
+    using Microsoft.Win32;
+
+    internal class LaunchOpenDialogCommand : CommandBase
+    {
+        private readonly string filter;
+        private readonly Action<OpenFileDialog> callback;
+
+        public LaunchOpenDialogCommand(string filter, Action<OpenFileDialog> callback)
+        {
+            this.filter = filter;
+            this.callback = callback;
+        }
+
+        public override void Execute(object parameter)
+        {
+            OpenFileDialog dialog = new OpenFileDialog()
+            {
+                Filter = this.filter
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                this.callback?.Invoke(dialog);
+            }
+        }
+    }
+}
