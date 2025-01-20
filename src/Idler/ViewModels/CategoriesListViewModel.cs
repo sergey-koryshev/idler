487cd220a1ddab145e2e0c5e2628971c5a84d941
Sergey Koryshev <sergey.koryshev@outlook.com>
2025-01-20 08:31:14 +0100

    Added view for categories list

---
+++ src/Idler/ViewModels/CategoriesListViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Idler.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Idler.ViewModels
+{
+    public class CategoriesListViewModel : BaseViewModel
+    {
+        private ObservableCollection<NoteCategory> categories;
+        private ICommand removeCategoryCommand;
+
+        public ObservableCollection<NoteCategory> Categories
+        {
+            get => categories;
+            set
+            {
+                categories = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand RemoveCategoryCommand
+        {
+            get => removeCategoryCommand;
+            set
+            {
+                removeCategoryCommand = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public CategoriesListViewModel(ObservableCollection<NoteCategory> categories)
+        {
+            this.Categories = categories;
+            this.RemoveCategoryCommand = new RemoveCategoryCommand(categories);
+        }
+    }
+}
