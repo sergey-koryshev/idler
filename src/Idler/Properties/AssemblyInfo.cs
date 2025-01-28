2c16aa94dabffdfcccbb7287b6982090f6e32ede
sergey-koryshev <sergey-koryshev@users.noreply.github.com>
2025-01-28 07:27:19 +0000

    [automated] Bumped version to 1.14.2.99 [skip ci]

--- src/Idler/Properties/AssemblyInfo.cs
+++ src/Idler/Properties/AssemblyInfo.cs
@@ -41,5 +41,5 @@ using System.Windows;
 )]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.14.1.98")]
-[assembly: AssemblyInformationalVersion("1.14.1")]
+[assembly: AssemblyFileVersion("1.14.2.99")]
+[assembly: AssemblyInformationalVersion("1.14.2")]
