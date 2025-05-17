fb5200eff149e1be150f1cb99b2c463ea0be89a6
Sergey Koryshev <sergey.koryshev@outlook.com>
2025-05-18 00:53:07 +0200

    Added BackgroundTasksManager and BackgroundTask classes

---
+++ src/Idler/Models/BackgroundTask.cs
@@ -0,0 +1,20 @@
+﻿namespace Idler.Models
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Represents a background task with a name and associated <see cref="Task"/>.
+    /// </summary>
+    public class BackgroundTask
+    {
+        /// <summary>
+        /// Gets or sets the name of the background task.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="Task"/> associated with this background task.
+        /// </summary>
+        public Task Action { get; set; }
+    }
+}
