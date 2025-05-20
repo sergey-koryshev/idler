f538f700a765806b7fa0674d355166120618acf8
Sergey Koryshev <sergey.koryshev@outlook.com>
2025-05-20 09:56:56 +0200

    Added new setting "IsAutoCategorizationEnabled"

--- src/Idler/Properties/Settings.Designer.cs
+++ src/Idler/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Idler.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -259,6 +259,18 @@ namespace Idler.Properties {
             }
             set {
                 this["IsSpellCheckEnabled"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool IsAutoCategorizationEnabled {
+            get {
+                return ((bool)(this["IsAutoCategorizationEnabled"]));
+            }
+            set {
+                this["IsAutoCategorizationEnabled"] = value;
             }
         }
     }
