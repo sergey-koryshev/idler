bf09632874749eac70aab0ae1c6ac4e77fa64a49
Sergey Koryshev <sergey.koryshev@outlook.com>
2025-05-23 07:05:32 +0200

    Added models to work with NLP model

---
+++ src/Idler/Models/PredictionResult.cs
@@ -0,0 +1,16 @@
+﻿namespace Idler.Models
+{
+    using Microsoft.ML.Data;
+
+    /// <summary>
+    /// Represents the result of a prediction made by the NLP model.
+    /// </summary>
+    public class PredictionResult
+    {
+        /// <summary>
+        /// Gets or sets the predicted category identifier based on the model's output.
+        /// </summary>
+        [ColumnName("PredictedLabel")]
+        public int PredictedCategoryId { get; set; }
+    }
+}
