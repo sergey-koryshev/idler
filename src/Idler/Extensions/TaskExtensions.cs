f0e1ae05d544b6475e8a992398484e16ba0e6058
Sergey Koryshev <sergey.koryshev@outlook.com>
2025-06-07 16:01:26 +0200

    Added IDispatcher interface for improved testability

--- src/Idler/Extensions/TaskExtensions.cs
+++ src/Idler/Extensions/TaskExtensions.cs
@@ -3,11 +3,45 @@
     using System;
     using System.Diagnostics;
     using System.Linq;
+    using System.Threading;
     using System.Threading.Tasks;
-    using System.Windows;
+    using Idler.Helpers;
+    using Idler.Interfaces;
 
     public static class TaskExtensions
     {
+        private static Lazy<IDispatcher> lazyDispatcher = new Lazy<IDispatcher>(() => new ApplicationDispatcher(), LazyThreadSafetyMode.ExecutionAndPublication);
+
+        private static IDispatcher CurrentDispatcher => lazyDispatcher.Value;
+
+        /// <summary>
+        /// Sets the dispatcher.
+        /// </summary>
+        /// <remarks>
+        /// The method is for unit testing purposes.
+        /// </remarks>
+        /// <param name="dispatcher">The dispatcher to use.</param>
+        public static void SetDispatcher(IDispatcher dispatcher)
+        {
+            if (dispatcher == null)
+            {
+                throw new ArgumentNullException(nameof(dispatcher));
+            }
+
+            Interlocked.Exchange(ref lazyDispatcher, new Lazy<IDispatcher>(() => dispatcher, LazyThreadSafetyMode.ExecutionAndPublication));
+        }
+
+        /// <summary>
+        /// Resets the dispatcher to the default implementation.
+        /// </summary>
+        /// <remarks>
+        /// The method is for unit testing purposes.
+        /// </remarks>
+        public static void ResetDispatcher()
+        {
+            Interlocked.Exchange(ref lazyDispatcher, new Lazy<IDispatcher>(() => new ApplicationDispatcher(), LazyThreadSafetyMode.ExecutionAndPublication));
+        }
+
         /// <param name="callback">An optional callback to invoke if the operation completes successfully.</param>
         /// <inheritdoc cref="SafeAsyncCall{T}(Task{T}, Action{T}, Action{bool}, Action{AggregateException})"/>
         public static Task SafeAsyncCall(this Task action, Action callback = null, Action<bool> setProcessing = null, Action<AggregateException> errorCallback = null)
@@ -51,7 +85,7 @@
         {
             if (setProcessing != null)
             {
-                Application.Current.Dispatcher.Invoke(setProcessing, true);
+                CurrentDispatcher.Invoke(setProcessing, true);
             }
 
             return action.ContinueWith((r) =>
@@ -61,14 +95,14 @@
                     bool isCanceled = action.Exception.Flatten().InnerExceptions.Any(ex => ex is OperationCanceledException);
                     if (isCanceled)
                     {
-                        Trace.TraceWarning($"Task was canceled.");
+                        Trace.TraceWarning($"Task was canceled due to OperationCanceledException.");
                     }
                     else
                     {
-                        Trace.TraceError($"Error has been occurred: {action.Exception}");
+                        Trace.TraceError($"Error has occurred: {action.Exception}");
                         if (errorCallback != null)
                         {
-                            Application.Current.Dispatcher.Invoke(errorCallback, action.Exception.InnerException);
+                            CurrentDispatcher.Invoke(errorCallback, action.Exception);
                         }
                     }
                 }
@@ -78,12 +112,12 @@
                 }
                 else if (success != null)
                 {
-                    Application.Current.Dispatcher.Invoke(success);
+                    CurrentDispatcher.Invoke(success);
                 }
 
                 if (setProcessing != null)
                 {
-                    Application.Current.Dispatcher.Invoke(setProcessing, false);
+                    CurrentDispatcher.Invoke(setProcessing, false);
                 }
             });
         }
---
+++ src/Idler/Interfaces/IDispatcher.cs
@@ -0,0 +1,14 @@
+﻿namespace Idler.Interfaces
+{
+    using System;
+    using System.Windows.Threading;
+
+    /// <summary>
+    /// Interface for abstracting dispatcher operations.
+    /// </summary>
+    public interface IDispatcher
+    {
+        /// <inheritdoc cref="Dispatcher.Invoke(Delegate, object[])" />
+        void Invoke(Delegate method, params object[] args);
+    }
+}
