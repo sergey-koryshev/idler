28fa90610b7b679eab845c34dce1ffb48656c670
Sergey Koryshev <sergey.koryshev@outlook.com>
2026-03-30 10:42:17 +0200

    Added base class for view model of closable dialogs

---
+++ src/Idler/ViewModels/BaseDialogViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Idler.ViewModels
+{
+    using System.Threading.Tasks;
+    using Idler.Components.PopupDialogHostControl;
+
+    public abstract class BaseDialogViewModel : BaseViewModel, IClosableDialog
+    {
+        public virtual Task CloseDailog()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
