b37ec39bfb57f3fd063fa207ef866668e978d0fe
Sergey Koryshev <sergey.koryshev@outlook.com>
2026-04-08 08:14:10 +0200

    Refactored class IClosableDialog, added documentation

--- src/Idler/Components/PopupDialogHostControl/IClosableDialog.cs
+++ src/Idler/Components/PopupDialogHostControl/IClosableDialog.cs
@@ -2,8 +2,16 @@
 {
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// Represents closable dialog.
+    /// </summary>
     public interface IClosableDialog
     {
-        Task CloseDailog();
+        /// <summary>
+        /// Performs asynchronous operations when a dialog is closing.
+        /// </summary>
+        /// <remarks>Ensure you handled all exceptions within the method.</remarks>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        Task OnDialogClosing();
     }
 }
