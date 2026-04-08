c504b1c3546952e80537625cb5d5cf0fe84ec883
Sergey Koryshev <sergey.koryshev@outlook.com>
2026-04-08 08:15:59 +0200

    Implemented DispatcherHelper in TaskExtensions

--- src/Idler/Extensions/TaskExtensions.cs
+++ src/Idler/Extensions/TaskExtensions.cs
@@ -6,42 +6,9 @@
     using System.Threading;
     using System.Threading.Tasks;
     using Idler.Helpers;
-    using Idler.Interfaces;
 
     public static class TaskExtensions
     {
-        private static Lazy<IDispatcher> lazyDispatcher = new Lazy<IDispatcher>(() => new ApplicationDispatcher(), LazyThreadSafetyMode.ExecutionAndPublication);
-
-        private static IDispatcher CurrentDispatcher => lazyDispatcher.Value;
-
-        /// <summary>
-        /// Sets the dispatcher.
-        /// </summary>
-        /// <remarks>
-        /// The method is for unit testing purposes.
-        /// </remarks>
-        /// <param name="dispatcher">The dispatcher to use.</param>
-        public static void SetDispatcher(IDispatcher dispatcher)
-        {
-            if (dispatcher == null)
-            {
-                throw new ArgumentNullException(nameof(dispatcher));
-            }
-
-            Interlocked.Exchange(ref lazyDispatcher, new Lazy<IDispatcher>(() => dispatcher, LazyThreadSafetyMode.ExecutionAndPublication));
-        }
-
-        /// <summary>
-        /// Resets the dispatcher to the default implementation.
-        /// </summary>
-        /// <remarks>
-        /// The method is for unit testing purposes.
-        /// </remarks>
-        public static void ResetDispatcher()
-        {
-            Interlocked.Exchange(ref lazyDispatcher, new Lazy<IDispatcher>(() => new ApplicationDispatcher(), LazyThreadSafetyMode.ExecutionAndPublication));
-        }
-
         /// <param name="callback">An optional callback to invoke if the operation completes successfully.</param>
         /// <inheritdoc cref="SafeAsyncCall{T}(Task{T}, Action{T}, Action{bool}, Action{AggregateException})"/>
         public static Task SafeAsyncCall(this Task action, Action<CancellationToken> callback = null, Action<bool> setProcessing = null, Action<AggregateException> errorCallback = null, CancellationToken? cancellationToken = null)
@@ -85,7 +52,7 @@
         {
             if (setProcessing != null)
             {
-                CurrentDispatcher.Invoke(setProcessing, true);
+                DispatcherHelper.CurrentDispatcher.Invoke(setProcessing, true);
             }
 
             return action.ContinueWith((r) =>
@@ -102,7 +69,7 @@
                         Trace.TraceError($"Error has occurred: {action.Exception}");
                         if (errorCallback != null)
                         {
-                            CurrentDispatcher.Invoke(errorCallback, action.Exception);
+                            DispatcherHelper.CurrentDispatcher.Invoke(errorCallback, action.Exception);
                         }
                     }
                 }
@@ -112,12 +79,12 @@
                 }
                 else if (success != null)
                 {
-                    CurrentDispatcher.Invoke(success);
+                    DispatcherHelper.CurrentDispatcher.Invoke(success);
                 }
 
                 if (setProcessing != null)
                 {
-                    CurrentDispatcher.Invoke(setProcessing, false);
+                    DispatcherHelper.CurrentDispatcher.Invoke(setProcessing, false);
                 }
             });
         }
