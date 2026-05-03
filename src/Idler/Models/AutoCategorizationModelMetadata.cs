605b1cace4d38d553d2d0041e765cbc7ede703d5
Sergey Koryshev <sergey.koryshev@outlook.com>
2026-05-03 17:00:34 +0200

    Added class for metadata

---
+++ src/Idler/Models/AutoCategorizationModelMetadata.cs
@@ -0,0 +1,18 @@
+﻿namespace Idler.Models
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Represents metadata of auto-categorization NLP model.
+    /// </summary>
+    [DataContract]
+    public class AutoCategorizationModelMetadata
+    {
+        /// <summary>
+        /// Gets or sets <see cref="DateTime"/> when the model was trained last time.
+        /// </summary>
+        [DataMember]
+        public DateTime? TrainedOn { get; set; }
+    }
+}
